f395c3ecff22ca9b80f12ec9a126eae62159bfff
root <root@debian9v1.fjeclot.net>
2018-05-04 20:36:56 +0200

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: https://github.com/marero1718daw2/pluginwp2_marrec.git
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Mario Recamales
+Author URI: https://github.com/marero1718daw2
+License: GNU GPLv3
+*/
+?>
